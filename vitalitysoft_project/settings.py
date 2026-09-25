21b88e8f1c400e451b57419949a03e3465792684
pavanagangam <pavanagangam@users.noreply.github.com>
2026-09-25 15:53:32 +0530

    Fix Vercel static asset rewrites & SQLite serverless path

--- vitalitysoft_project/settings.py
+++ vitalitysoft_project/settings.py
@@ -56,7 +56,7 @@ WSGI_APPLICATION = 'vitalitysoft_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3' if os.environ.get('VERCEL') else BASE_DIR / 'db.sqlite3',
     }
 }
 
